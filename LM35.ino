55d56f6264134732f96a80e0f18319e2b5cac523
nikhil73533 <55098765+nikhil73533@users.noreply.github.com>
2021-10-13 22:59:56 +0530

    Update LM35.ino
    This is code for making reader fool

--- LM35.ino
+++ LM35.ino
@@ -1,2 +1,33 @@
 // Code to use LM35 temperature sensor with NodeMCU
 
+int Ldr_pin  = A0;
+int led_pin  = D0;
+int senser_value = 0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(Ldr_pin,INPUT);
+  pinMode(led_pin,OUTPUT);
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  senser_value = analogRead(Ldr_pin);
+  Serial.println(senser_value);
+  if(senser_value <100)
+  {
+     digitalWrite(led_pin,HIGH);
+     Serial.println("Led is On");
+    }
+   else
+   {
+     digitalWrite(led_pin,LOW);
+     Serial.println("Led is Off");
+   }
+
+    
+  delay(1000);
+ 
+  
+}
